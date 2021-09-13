40bfccecaa5ebac1a0f51746cc116047756a3dbc
Shadman Shakib <sm.shakib007@gmail.com>
2021-09-13 10:50:46 +0600

    swiperight and left

--- components/Cards/RoomCard.tsx
+++ components/Cards/RoomCard.tsx
@@ -1,19 +1,7 @@
 import React from "react";
 import { View, Text, Image, FlatList, Button } from "native-base";
-import { PanGestureHandler, State } from "react-native-gesture-handler";
+import { Dimensions, Animated } from "react-native";
 
-import Animated, {
-  add,
-  clockRunning,
-  cond,
-  debug,
-  divide,
-  eq,
-  floor,
-  not,
-  set,
-  useCode,
-} from "react-native-reanimated";
 const URLs = [
   "https://a0.muscache.com/im/pictures/miso/Hosting-48837541/original/67b1be92-42e3-47d7-bfc8-cbfeb59428da.jpeg?im_w=720",
 
@@ -23,28 +11,40 @@ const URLs = [
 ];
 
 export default function RoomCard() {
+  const { width, height } = Dimensions.get("window");
   const [state, setState] = React.useState(0);
-  const handlePress = () => {
+
+  const handleRightSwipe = () => {
     if (state === 0 || state < 2) setState(state + 1);
     else {
       setState(0);
     }
   };
+  const handleLeftSwipe = () => {
+    if (state === 2 || state > 0) setState(state - 1);
+    else setState(2);
+  };
+
+  const fadeIn = React.useRef(new Animated.Value(0));
   return (
     <View>
       <View
+        mx="6"
         onMoveShouldSetResponder={() => true}
-        onResponderGrant={handlePress}
+        onResponderGrant={(e: any) => {
+          if (e.nativeEvent.locationX > width / 2) handleRightSwipe();
+          else {
+            handleLeftSwipe();
+          }
+        }}
       >
         <Image
           alt="2hfhd"
           height="366px"
-          width="366px"
+          width="100%"
           source={{ uri: URLs[state] }}
         />
       </View>
-
-      <Button onPress={handlePress}>Next</Button>
     </View>
   );
 }
