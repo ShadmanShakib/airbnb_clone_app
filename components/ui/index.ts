5525ad3ce193058810a8d67d91453bf1bc364157
Shadman Shakib <sm.shakib007@gmail.com>
2021-09-18 10:24:56 +0600

    google places api removed

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,5 +1,4 @@
 export { default as theme } from "./Theme";
 export { default as Hero } from "./Hero";
 export { default as WithButton } from "./WithButton";
-export { default as GooglePlacesInput } from "./GooglePlacesInput";
 export { default as FlexibleHeader } from "./FlexibleHeader";
--- screens/user/SearchModal.tsx
+++ screens/user/SearchModal.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { Box, Text, Input, ScrollView, Button } from "native-base";
-import { GooglePlacesInput } from "../../components/ui";
 export default function SearchModal() {
+  const url =
+    "/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=";
+  const fetchPlaces = () => {
+    fetch(
+      "https://maps.google.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=AIzaSyBWjpdfGbxfUtS41DCn1mBFxu8oAMBX6H4",
+      {
+        headers: {},
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
   return (
     <Box pt="10" h="100%">
-      <GooglePlacesInput />
       <Text>GO ANYWHERE, ANYTIME</Text>
-      <Button bg="white" shadow={3} rounded={4}>
+      <Button onPress={fetchPlaces} bg="white" shadow={3} rounded={4}>
         <Text>I'm flexible</Text>
       </Button>
       <ScrollView></ScrollView>
